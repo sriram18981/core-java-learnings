707688ceb802ecf1de3a50157b283d16a6799289
Sri Ramachandra Murthy Tanikella <sriramachandramurthy.tanikella@broadcom.com>
2019-01-23 20:53:49 +0530

    Another stream tester

--- src/learnings/sriram/collections/streams/StreamsTester1.java
+++ src/learnings/sriram/collections/streams/StreamsTester1.java
@@ -1,14 +1,12 @@
+package learnings.sriram.collections.streams;
+
 import java.util.Arrays;
-import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
-public class StreamsTester1{
-    static int[] integers = {1,2,3,4,5,6,7,8,9,10};
+public class StreamsTester1 {
+	static int[] integers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-    public static void main(String[] args){
-       Stream<int[]> stream = Stream.of(integers);
-       stream.forEach(System.out::println);
-    }
+	public static void main(String[] args) {
+		Arrays.stream(integers).forEach(System.out::println);
+	}
 
-    
 }
